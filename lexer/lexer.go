6c6dd99b1529d5d9dcee87b33b010c7c6b63bcfd
Kevin <llewellyn.kevin@yahoo.com>
2025-02-27 11:27:17 -0700

    feat: lex single and double symbols

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,8 +1,137 @@
 package lexer
 
-type Lexer struct {
+import (
+	"io"
+)
+
+type ReadSeeker interface {
+	io.Reader
+	io.Seeker
 }
 
-func (l *Lexer) NextToken() Token {
-	return Token{ILLEGAL, "foobar"}
+type Lexer struct {
+	Input  ReadSeeker
+	offset int
+}
+
+func NewLexer(input ReadSeeker) *Lexer {
+	return &Lexer{
+		Input:  input,
+		offset: 0,
+	}
+}
+
+func (l *Lexer) NextToken() (output Token) {
+	var buffer []byte
+
+	for output.Type == "" {
+		nextChar := make([]byte, 1)
+		n, err := l.Input.Read(nextChar)
+		l.offset += n
+
+		if err == io.EOF {
+			output = Token{EOF, ""}
+			return
+		}
+
+		buffer = append(buffer, nextChar...)
+		switch string(buffer) {
+		// Single Chars
+		case "*":
+			output = Token{MULT, string(buffer)}
+		case "/":
+			output = Token{DIV, string(buffer)}
+		case "%":
+			output = Token{MOD, string(buffer)}
+		case "=":
+			output = Token{EQ, string(buffer)}
+		case "^":
+			output = Token{BXOR, string(buffer)}
+		case "&":
+			output = Token{BAND, string(buffer)}
+		case "|":
+			output = Token{BOR, string(buffer)}
+
+			// Single / Double
+		case "+": // + or ++
+			if eof, peeked, advancer := l.peekChar(); eof || peeked != byte('+') {
+				output = Token{ADD, "+"}
+			} else {
+				output = Token{INC, "++"}
+				advancer()
+			}
+		case "-": // - or -- or ->
+			eof, peeked, advancer := l.peekChar()
+			if eof {
+				output = Token{SUB, "-"}
+			}
+
+			switch peeked {
+			case byte('-'):
+				output = Token{DEC, "--"}
+				advancer()
+			case byte('>'):
+				output = Token{ASSIGN, "->"}
+				advancer()
+			default:
+				output = Token{SUB, "-"}
+			}
+		case "<": // < or <= or <> or <<
+			eof, peeked, advancer := l.peekChar()
+			if eof {
+				output = Token{LT, "<"}
+			}
+
+			switch peeked {
+			case byte('='):
+				output = Token{LTEQ, "<="}
+				advancer()
+			case byte('<'):
+				output = Token{LSHIFT, "<<"}
+				advancer()
+			case byte('>'):
+				output = Token{SWAP, "<>"}
+				advancer()
+			default:
+				output = Token{LT, "<"}
+			}
+		case ">": // > or >= or >>
+			eof, peeked, advancer := l.peekChar()
+			if eof {
+				output = Token{GT, ">"}
+			}
+
+			switch peeked {
+			case byte('='):
+				output = Token{GTEQ, ">="}
+				advancer()
+			case byte('>'):
+				output = Token{RSHIFT, ">>"}
+				advancer()
+			default:
+				output = Token{GT, ">"}
+			}
+
+			// Single / Identifiers
+		case ".": // . or .IDENT
+			output = Token{DUP, string(buffer)}
+		}
+	}
+
+	return
+}
+
+func (l *Lexer) peekChar() (bool, byte, func() (int64, error)) {
+	advancer := func() (int64, error) {
+		l.offset += 1
+		return l.Input.Seek(int64(l.offset), 0)
+	}
+
+	char := make([]byte, 1)
+	l.Input.Read(char)
+	_, err := l.Input.Seek(int64(l.offset), 0)
+	if err == io.EOF {
+		return true, byte(0), advancer
+	}
+	return false, char[0], advancer
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,12 +1,63 @@
 package lexer
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestNextToken(t *testing.T) {
-	lexer := Lexer{}
+	reader := strings.NewReader("%")
+	lexer := NewLexer(reader)
+
+	expected := []Token{{MOD, "%"}}
+
+	for _, token := range expected {
+		if actual := lexer.NextToken(); actual != token {
+			t.Fatalf("Unexpected token. Want %v, Got %v", token, actual)
+		}
+	}
+}
+
+func TestSingleCharOps(t *testing.T) {
+	reader := strings.NewReader("+-*/%=&|^")
+	lexer := NewLexer(reader)
 
 	expected := []Token{
-		{ILLEGAL, "foobar"},
+		{ADD, "+"},
+		{SUB, "-"},
+		{MULT, "*"},
+		{DIV, "/"},
+		{MOD, "%"},
+		{EQ, "="},
+		{BAND, "&"},
+		{BOR, "|"},
+		{BXOR, "^"},
+	}
+
+	for _, token := range expected {
+		if actual := lexer.NextToken(); actual != token {
+			t.Fatalf("Unexpected token. Want %v, Got %v", token, actual)
+		}
+	}
+}
+
+func TestDoubleCharOps(t *testing.T) {
+	reader := strings.NewReader("><++--+->-<<<>>><=>=")
+	lexer := NewLexer(reader)
+
+	expected := []Token{
+		{GT, ">"},
+		{LT, "<"},
+		{INC, "++"},
+		{DEC, "--"},
+		{ADD, "+"},
+		{ASSIGN, "->"},
+		{SUB, "-"},
+		{LSHIFT, "<<"},
+		{SWAP, "<>"},
+		{RSHIFT, ">>"},
+		{LTEQ, "<="},
+		{GTEQ, ">="},
 	}
 
 	for _, token := range expected {
