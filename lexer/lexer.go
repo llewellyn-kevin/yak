9f32855b67d0cfa3751e08b4d2846dbc5acedb61
Kevin <llewellyn.kevin@yahoo.com>
2025-07-31 23:13:41 -0600

    chore: documentation and organization

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -21,34 +21,6 @@ func NewLexer(input ReadSeeker) *Lexer {
 	}
 }
 
-func (l *Lexer) nextChar() ([]byte, error) {
-	nextChar := make([]byte, 1)
-	n, err := l.Input.Read(nextChar)
-	l.setOffset(l.offset + int64(n))
-	return nextChar, err
-}
-
-func (l *Lexer) nextWithoutWhitespace() ([]byte, error) {
-	nextChar := []byte{' '}
-	var err error
-	for len(nextChar) == 1 && (nextChar[0] == ' ' || nextChar[0] == '\t' || nextChar[0] == '\n' || nextChar[0] == '\r') && err == nil {
-		nextChar, err = l.nextChar()
-	}
-	return nextChar, err
-}
-
-func (l *Lexer) setOffset(offset int64) {
-	if offset < 0 {
-		offset = 0
-	}
-	l.offset = offset
-	l.Input.Seek(l.offset, io.SeekStart)
-}
-
-func (l *Lexer) goBack(amount int64) {
-	l.setOffset(l.offset - amount)
-}
-
 // A map of byte arrays representing keywords to their correct token.
 var Keywords = map[string]TokenType{
 	"if":      IF,
@@ -70,60 +42,6 @@ var Keywords = map[string]TokenType{
 	"yakin":   YAKIN,
 	"yakup":   YAKUP,
 	"yakout!": LITERAL_YAKOUT,
-}
-
-// A function that takes a char array and returns a token.
-type tokenOutputter func(ch []byte, l *Lexer) Token
-
-// Maps characters to their tokens via token outputters.
-type tMap map[string]tokenOutputter
-
-// Returns a closure that returns the token for any symbol that is only a
-// single character or is on the last character, so no lookahead is needed.
-func terminalToken(t TokenType) tokenOutputter {
-	return func(ch []byte, _ *Lexer) Token {
-		return Token{t, string(ch)}
-	}
-}
-
-// Struct used to hold logical checks where matching a token is more complex
-// than matching a single character.
-type logicalHandler struct {
-	check   func(peek []byte) bool
-	scanner tokenOutputter
-}
-
-// Returns a closure to handle reading multi-character tokens.
-// fallback is the token to return if no match is found in the map.
-// nextMap is the map of functions that return tokens with the symbols as
-// their keys.
-// lHandlers are an optional list of logicalHandlers. If the check function
-// for the handler returns true, the scanner function will be executed to get
-// the token. Otherwise this will be skipped. This can be used for checks where
-// multiple characters are valid for the given token.
-func recursiveToken(fallback tokenOutputter, nextMap tMap, lHandlers ...logicalHandler) tokenOutputter {
-	return func(ch []byte, l *Lexer) Token {
-		n, err := l.nextChar()
-		if err == io.EOF {
-			return fallback(ch, l)
-		}
-
-		out, exists := nextMap[string(n)]
-		if exists {
-			ch = append(ch, n...)
-			return out(ch, l)
-		}
-
-		for _, h := range lHandlers {
-			if h.check(n) {
-				ch = append(ch, n...)
-				return h.scanner(ch, l)
-			}
-		}
-
-		l.goBack(1)
-		return fallback(ch, l)
-	}
 }
 
 // Maps byte arrays representing tokens to their token outputers.
@@ -195,6 +113,60 @@ var tokenMap = tMap{
 	),
 }
 
+// A function that takes a char array and returns a token.
+type tokenOutputter func(ch []byte, l *Lexer) Token
+
+// Maps characters to their tokens via token outputters.
+type tMap map[string]tokenOutputter
+
+// Returns a closure that returns the token for any symbol that is only a
+// single character or is on the last character, so no lookahead is needed.
+func terminalToken(t TokenType) tokenOutputter {
+	return func(ch []byte, _ *Lexer) Token {
+		return Token{t, string(ch)}
+	}
+}
+
+// Struct used to hold logical checks where matching a token is more complex
+// than matching a single character.
+type logicalHandler struct {
+	check   func(peek []byte) bool
+	scanner tokenOutputter
+}
+
+// Returns a closure to handle reading multi-character tokens.
+// fallback is the token to return if no match is found in the map.
+// nextMap is the map of functions that return tokens with the symbols as
+// their keys.
+// lHandlers are an optional list of logicalHandlers. If the check function
+// for the handler returns true, the scanner function will be executed to get
+// the token. Otherwise this will be skipped. This can be used for checks where
+// multiple characters are valid for the given token.
+func recursiveToken(fallback tokenOutputter, nextMap tMap, lHandlers ...logicalHandler) tokenOutputter {
+	return func(ch []byte, l *Lexer) Token {
+		n, err := l.nextChar()
+		if err == io.EOF {
+			return fallback(ch, l)
+		}
+
+		out, exists := nextMap[string(n)]
+		if exists {
+			ch = append(ch, n...)
+			return out(ch, l)
+		}
+
+		for _, h := range lHandlers {
+			if h.check(n) {
+				ch = append(ch, n...)
+				return h.scanner(ch, l)
+			}
+		}
+
+		l.goBack(1)
+		return fallback(ch, l)
+	}
+}
+
 // Get the next semantically relevant token.
 func (l *Lexer) NextToken() (output Token) {
 	for output.Type == "" {
@@ -223,6 +195,39 @@ func (l *Lexer) NextToken() (output Token) {
 	return
 }
 
+// Lexer helper that returns the next rune as a byte array
+func (l *Lexer) nextChar() ([]byte, error) {
+	nextChar := make([]byte, 1)
+	n, err := l.Input.Read(nextChar)
+	l.setOffset(l.offset + int64(n))
+	return nextChar, err
+}
+
+// Lexer helper that gets the next char but skips any whitespace
+func (l *Lexer) nextWithoutWhitespace() ([]byte, error) {
+	nextChar := []byte{' '}
+	var err error
+	for len(nextChar) == 1 && (nextChar[0] == ' ' || nextChar[0] == '\t' || nextChar[0] == '\n' || nextChar[0] == '\r') && err == nil {
+		nextChar, err = l.nextChar()
+	}
+	return nextChar, err
+}
+
+// Lexer helper that jumps the file reader to a specific offset
+func (l *Lexer) setOffset(offset int64) {
+	if offset < 0 {
+		offset = 0
+	}
+	l.offset = offset
+	l.Input.Seek(l.offset, io.SeekStart)
+}
+
+// Lexer helper that goes back the specified number of bytes in the reader
+func (l *Lexer) goBack(amount int64) {
+	l.setOffset(l.offset - amount)
+}
+
+// Returns whether the rune contained in the byte array is a letter or underscore
 func isLetter(ch []byte) bool {
 	if len(ch) > len([]byte{'a'}) {
 		return false
@@ -231,6 +236,7 @@ func isLetter(ch []byte) bool {
 	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || b == '_'
 }
 
+// Returns whether or not the rune contained in the byte array is a number
 func isDigit(ch []byte) bool {
 	if len(ch) > len([]byte{'0'}) {
 		return false
@@ -239,6 +245,9 @@ func isDigit(ch []byte) bool {
 	return b >= '0' && b <= '9'
 }
 
+// See if the given string is a language keyword. And return that keyword's
+// token if so. Otherwise, return the identifier token with the string as
+// content.
 func lookupIdentifier(ident string) TokenType {
 	if tok, ok := Keywords[ident]; ok {
 		return tok
@@ -246,6 +255,8 @@ func lookupIdentifier(ident string) TokenType {
 	return IDENT
 }
 
+// Keep reading in symbols in the lexer until you hit a non valid id char.
+// Return the resulting keyword or identifier token.
 func (l *Lexer) readIdentifier(buffer []byte) Token {
 	for {
 		nextChar := make([]byte, 1)
@@ -270,6 +281,8 @@ func (l *Lexer) readIdentifier(buffer []byte) Token {
 	return Token{lookupIdentifier(string(s)), string(s)}
 }
 
+// Keep reading the file until an invalid input for a number. Return the correct
+// number literal token (INT or FLOAT)
 func (l *Lexer) readNumber(buffer []byte) Token {
 	isFloat := false
 	if buffer[0] == '.' || (buffer[0] == '-' && buffer[1] == '.') {
@@ -302,6 +315,8 @@ func (l *Lexer) readNumber(buffer []byte) Token {
 	return Token{INT, string(buffer)}
 }
 
+// Keep reading the file until an invalid symbol input. Then return the symbol
+// token with the read content.
 func (l *Lexer) readSymbol(buffer []byte) Token {
 	for {
 		n, err := l.nextChar()
